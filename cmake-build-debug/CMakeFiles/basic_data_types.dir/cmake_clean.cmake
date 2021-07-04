65339b1da6219cbaee8d9ce1375756a6be7f6732
Terence Minerbrook <terence.minerbrook@gmail.com>
2021-07-04 17:54:46 -0400

    first commit

---
+++ cmake-build-debug/CMakeFiles/basic_data_types.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/basic_data_types.dir/main.cpp.obj"
+  "basic_data_types.exe"
+  "basic_data_types.exe.manifest"
+  "basic_data_types.lib"
+  "basic_data_types.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/basic_data_types.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+
+
+
+        int a;
+        long b;
+        char c;
+        float d;
+        double e;
+
+        scanf("%d %ld %c %f %lf ", &a, &b, &c, &d, &e);
+        printf("%d\n%ld\n%c\n%.3f\n%.9lf \n", a, b, c, d, e);
+
+
+//char ch = 'd';
+//double d = 234.432;
+//printf("%c %lf", ch, d);
+
+
+
+        /*
+
+          int t = 3;
+          long l = 12345678912345;
+          char c ='a';
+          float f = 334.23;
+          double d=14049.30493;
+
+          //printf("%d \n %ld \n %c \n %f \n %lf \n", t,l,c,f,d);
+
+
+
+            printf("%d \n", t);
+          printf("%ld \n", l);
+          printf("%c \n", c);
+          printf("%.3f \n", f);
+          printf("%.9lf \n", d);
+
+        */
+
+
+
+//char ch = 'd';
+//double d = 234.432;
+//printf("%c %lf", ch, d);
+
+
+
+
+
+/*
+    int t = 3;
+    long l = 12345678912345;
+    char c ='a';
+    float f = 334.23;
+    double d=14049.30493;
+
+
+
+    printf("%d \n", t);
+    printf("%ld \n", l);
+    printf("%c \n", c);
+    printf("%f \n", f);
+    printf("%lf \n", d);
+*/
+
+
+
+
+
+
+    // Complete the code.
+    return 0;
+}
